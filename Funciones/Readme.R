4bdbf35a96801fd42b3e178483c86936a4e7b2ae
sehercab <sebas92hc@gmail.com>
2021-05-28 15:50:01 +0200

    Create Readme.R

---
+++ Funciones/Readme.R
@@ -0,0 +1,20 @@
+
+
+
+Todo el código necesario para el uso de GapeR se puede encontrar en:
+    
+https://github.com/Sebas92hc/GapeR_tesis
+
+En este mismo enlace se pueden encontrar los archivos de datos "Dataindividuals_2s_AVG.txt" y 
+"Datasensors_0001.txt". Debido a su extensión y tamaño, "Dataindividuals_2s_AVG.txt" solo 
+contiene una submuestra de 10 días. En caso de necesitarse el resto de los datos, estos pueden
+solicitarse a través de mi correo personal sebas92hc@gmail.com.
+
+
+El script SebasDisplay.R contiene el código necesario para leer los archivos de datos e
+iniciar el programa.
+
+
+
+
+
