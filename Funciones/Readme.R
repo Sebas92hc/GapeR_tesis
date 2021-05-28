c47c33140572f00fef1de6bc14921a14212c05b4
sehercab <sebas92hc@gmail.com>
2021-05-28 15:58:45 +0200

    Final

--- Funciones/Readme.R
+++ Funciones/Readme.R
@@ -7,7 +7,7 @@ https://github.com/Sebas92hc/GapeR_tesis
 
 En este mismo enlace se pueden encontrar los archivos de datos "Dataindividuals_2s_AVG.txt" y 
 "Datasensors_0001.txt". Debido a su extensión y tamaño, "Dataindividuals_2s_AVG.txt" solo 
-contiene una submuestra de 10 días. En caso de necesitarse el resto de los datos, estos pueden
+contiene una submuestra de 6 días. En caso de necesitarse el resto de los datos, estos pueden
 solicitarse a través de mi correo personal sebas92hc@gmail.com.
 
 
